c6f6ad6838aff740271e1681277a1f0ae8cc94a7
Ngoc Tran <ngochtran2015@gmail.com>
2024-02-26 23:04:22 -0800

    tests

--- calculator/tests/01_example.sh
+++ calculator/tests/01_example.sh
@@ -29,3 +29,27 @@ if $CALCULATOR 3 @ 2; then  # If the return code of $PROGRAM is zero (i.e. succe
   echo 'ERROR! An invalid run of the application (3 @ 2) apparently succeeded?!'
   exit 1
 fi
+
+# Test 04: Ensure subtraction works
+if [[ $($CALCULATOR 10 - 2) -ne 8 ]]; then
+  echo 'ERROR! Subtraction (10 - 2) failed to produce 8 as an output!'
+  exit 1
+fi
+
+# Test 05: Ensure multiplication works
+if [[ $($CALCULATOR 5 '*' 4) -ne 20 ]]; then
+  echo 'ERROR! Multiplication (5 * 4) failed to produce 20 as an output!'
+  exit 1
+fi
+
+# Test 06: Ensure division works
+if [[ $($CALCULATOR 4 / 2) -ne 2 ]]; then
+  echo 'ERROR! Division (4 / 2) failed to produce 2 as an output!'
+  exit 1
+fi
+
+# Test 07: Ensure addition works
+if [[ $($CALCULATOR 3 + 3) -ne 6 ]]; then
+  echo 'ERROR! Subtraction (3 + 3) failed to produce 6 as an output!'
+  exit 1
+fi
